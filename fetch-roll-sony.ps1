6fdcc716e7ab25d4c0e7f4a60ac3c00bf43e7c3b
Hugo_古学冬 <idong1111@gmail.com>
2024-02-04 19:02:07 +0800

    Update fetch-roll-sony.ps1

--- fetch-roll-sony.ps1
+++ fetch-roll-sony.ps1
@@ -28,35 +28,30 @@ function doArchive {
   }
   timeout /t -1
 }
-function main {
-  param (
-  )
-  $rollDrives = New-Object System.Collections.ArrayList
-  # 获取所有盘符
-  $drives = Get-PSDrive -PSProvider FileSystem
-  # 遍历每个盘符
-  foreach ($drive in $drives) {
-    # 寻找索尼相机卡，拼接文件路径
-    $file = Join-Path -Path $drive.Root -ChildPath "/PRIVATE/SONY/SONYCARD.IND"
-    # 判断文件是否存在
-    if (Test-Path -Path $file) {
-      # 底片盘路径
-      $rollDrives.Add($drive.Root)
-    }
-  }
-  # 检查是否找到了相机卡
-  if (($null -eq $rollDrives) -or ($rollDrives.count -eq 0)) {
-    # 输出错误信息
-    Write-Output "Camara roll drive is not found."
-    timeout /t -1
-  }
-  else {
-    doArchive
-    foreach ($rollDrive in $rollDrives) {
-      # 复制新文件
-      doCopy($rollDrive, $backupPath)
-    }
-    doArchive
+$rollDrives = New-Object System.Collections.ArrayList
+# 获取所有盘符
+$drives = Get-PSDrive -PSProvider FileSystem
+# 遍历每个盘符
+foreach ($drive in $drives) {
+  # 寻找索尼相机卡，拼接文件路径
+  $file = Join-Path -Path $drive.Root -ChildPath "/PRIVATE/SONY/SONYCARD.IND"
+  # 判断文件是否存在
+  if (Test-Path -Path $file) {
+    # 底片盘路径
+    $rollDrives.Add($drive.Root)
   }
 }
-main
+# 检查是否找到了相机卡
+if (($null -eq $rollDrives) -or ($rollDrives.count -eq 0)) {
+  # 输出错误信息
+  Write-Output "Camara roll drive is not found."
+  timeout /t -1
+}
+else {
+  doArchive
+  foreach ($rollDrive in $rollDrives) {
+    # 复制新文件
+    doCopy($rollDrive, $backupPath)
+  }
+  doArchive
+}
