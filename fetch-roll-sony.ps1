72ffaed3b0aca98161dbc40f8196cd8aee24c257
Hugo_古学冬 <idong1111@gmail.com>
2024-02-04 18:10:19 +0800

    feat: add sony camara roll fetch script

---
+++ fetch-roll-sony.ps1
@@ -0,0 +1,55 @@
+param(
+  [Alias("b")]
+  # 备份路径
+  [string]$backupPath = ".\"
+)
+# 获取所有盘符
+$drives = Get-PSDrive -PSProvider FileSystem
+# 遍历每个盘符
+foreach ($drive in $drives) {
+  # 寻找索尼相机卡，拼接文件路径
+  $file = Join-Path -Path $drive.Root -ChildPath "/PRIVATE/SONY/SONYCARD.IND"
+  # 判断文件是否存在
+  if (Test-Path -Path $file) {
+    # 底片盘路径
+    $rollDrive = $drive.Root
+    # 复制新文件
+    doCopy($rollDrive, $backupPath)
+  }
+}
+# 检查是否找到了相机卡
+if (-not $rollDrive) {
+  # 输出错误信息
+  Write-Output "Camara roll drive is not found."
+  timeout /t -1
+}
+else {
+  doArchive
+}
+function doCopy {
+  param (
+    $src,
+    $dest
+  )
+  robocopy $src $dest /e /xo /copy:dat 
+}
+function doArchive {
+  $videoFolderPath = "$backupPath\PRIVATE\"
+  # 检查视频目录是否存在
+  if (((Test-Path -Path $videoFolderPath))) {
+    $videoFolder = Get-Item "$videoFolderPath"
+    $dateString = $date.ToString("yyMMddHHmm")
+    if ((-not (Test-Path -Path ($videoFolderPath.TrimEnd('\') + "-" + $dateString)))) {
+      $newName = "PRIVATE-" + $dateString
+      Write-Output "Video directory is found. Archiving."
+      Write-Output "Video directory archive time: "$dateString
+      # 归档视频目录
+      Rename-Item -LiteralPath $videoFolder -NewName $newName
+    } 
+  }
+  else {
+    # 目录不存在，跳过
+    Write-Output "The video directory does not exist. Archive skipping."
+  }
+  timeout /t -1
+}
