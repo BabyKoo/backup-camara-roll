dedf06e344451f6423059e3b36e2b33bb27e7c34
Hugo_古学冬 <idong1111@gmail.com>
2024-02-04 19:01:07 +0800

    Update fetch-roll-sony.ps1

--- fetch-roll-sony.ps1
+++ fetch-roll-sony.ps1
@@ -3,31 +3,6 @@ param(
   # 备份路径
   [string]$backupPath = ".\"
 )
-# 获取所有盘符
-$drives = Get-PSDrive -PSProvider FileSystem
-# 遍历每个盘符
-foreach ($drive in $drives) {
-  # 寻找索尼相机卡，拼接文件路径
-  $file = Join-Path -Path $drive.Root -ChildPath "/PRIVATE/SONY/SONYCARD.IND"
-  # 判断文件是否存在
-  if (Test-Path -Path $file) {
-    # 底片盘路径
-    $rollDrives.Add($drive.Root)
-  }
-}
-# 检查是否找到了相机卡
-if (-not $rollDrives -or ($rollDrives.count -eq 0)) {
-  # 输出错误信息
-  Write-Output "Camara roll drive is not found."
-  timeout /t -1
-}
-else {
-  foreach ($rollDrive in $rollDrives) {
-    # 复制新文件
-    doCopy($rollDrive, $backupPath)
-  }
-  doArchive
-}
 function doCopy {
   param (
     $src,
@@ -40,14 +15,12 @@ function doArchive {
   # 检查视频目录是否存在
   if (((Test-Path -Path $videoFolderPath))) {
     $videoFolder = Get-Item "$videoFolderPath"
-    $dateString = $date.ToString("yyMMddHHmm")
-    if ((-not (Test-Path -Path ($videoFolderPath.TrimEnd('\') + "-" + $dateString)))) {
-      $newName = "PRIVATE-" + $dateString
-      Write-Output "Video directory is found. Archiving."
-      Write-Output "Video directory archive time: "$dateString
-      # 归档视频目录
-      Rename-Item -LiteralPath $videoFolder -NewName $newName
-    } 
+    $lastWriteTimeString = $videoFolder.LastWriteTime.ToString('yyMMddHHmm')
+    $archiveName = "PRIVATE-" + $lastWriteTimeString
+    Write-Output "Video directory is found. Archiving."
+    Write-Output "Video directory archive sign: "$lastWriteTimeString
+    # 归档视频目录，如果最后写入时间相同，则视为同一目录
+    Move-Item -Path $videoFolderPath -Destination ($backupPath + '\' + $archiveName)
   }
   else {
     # 目录不存在，跳过
@@ -55,3 +28,35 @@ function doArchive {
   }
   timeout /t -1
 }
+function main {
+  param (
+  )
+  $rollDrives = New-Object System.Collections.ArrayList
+  # 获取所有盘符
+  $drives = Get-PSDrive -PSProvider FileSystem
+  # 遍历每个盘符
+  foreach ($drive in $drives) {
+    # 寻找索尼相机卡，拼接文件路径
+    $file = Join-Path -Path $drive.Root -ChildPath "/PRIVATE/SONY/SONYCARD.IND"
+    # 判断文件是否存在
+    if (Test-Path -Path $file) {
+      # 底片盘路径
+      $rollDrives.Add($drive.Root)
+    }
+  }
+  # 检查是否找到了相机卡
+  if (($null -eq $rollDrives) -or ($rollDrives.count -eq 0)) {
+    # 输出错误信息
+    Write-Output "Camara roll drive is not found."
+    timeout /t -1
+  }
+  else {
+    doArchive
+    foreach ($rollDrive in $rollDrives) {
+      # 复制新文件
+      doCopy($rollDrive, $backupPath)
+    }
+    doArchive
+  }
+}
+main
