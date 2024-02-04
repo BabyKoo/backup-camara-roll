b7bf4823fbfe1c5bc84c84ed6e6b0d8ef0041744
Hugo_古学冬 <idong1111@gmail.com>
2024-02-04 18:23:23 +0800

    Update fetch-roll-sony.ps1

--- fetch-roll-sony.ps1
+++ fetch-roll-sony.ps1
@@ -12,18 +12,20 @@ foreach ($drive in $drives) {
   # 判断文件是否存在
   if (Test-Path -Path $file) {
     # 底片盘路径
-    $rollDrive = $drive.Root
-    # 复制新文件
-    doCopy($rollDrive, $backupPath)
+    $rollDrives.Add($drive.Root)
   }
 }
 # 检查是否找到了相机卡
-if (-not $rollDrive) {
+if (-not $rollDrives -or ($rollDrives.count -eq 0)) {
   # 输出错误信息
   Write-Output "Camara roll drive is not found."
   timeout /t -1
 }
 else {
+  foreach ($rollDrive in $rollDrives) {
+    # 复制新文件
+    doCopy($rollDrive, $backupPath)
+  }
   doArchive
 }
 function doCopy {
